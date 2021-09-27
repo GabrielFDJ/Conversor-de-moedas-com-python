1510b528de470e072445fa25e0d99a695f4f215d
GabrielFDJ <gabrielferreiradj2014@gmail.com>
2021-09-27 12:18:49 -0300

    adicionando laço principal com estrutura

--- Conversor_de_moedas.py
+++ Conversor_de_moedas.py
@@ -12,9 +12,65 @@ def print_moeda():
 
 def mensagem_de_erro():
     print('\033[1;33;41m'+'Ops! Entrada Inválida ... tente novamente com um valor válido'+'\033[m')    
-   
+ 
+#inicio
+while True:
+    formatacao_titulo(' BEM VINDO AO CONVERSOR DE MOEDA')
+    formatacao_titulo('- INSIRA O VALOR A SER CAMBIADO\n- INSIRA A MOEDA INICIAL\n- INSIRA A MOEDA DE CAMBIO')
+    formatacao_titulo('VALOR INICIAL')
 
-    
-            
+    while True:
+        try:
+            valor_inicial = float(input("Insira  o valor a ser cambiado: \n"))
+            break
+        except ValueError:
+            mensagem_de_erro()
+
+    formatacao_titulo('SELECIONE A MOEDA INICIAL')
+    cod_moeda_inicial = None
+    while cod_moeda_inicial not in (1,2,3):
+        print_moeda()
+        try:
+            cod_moeda_inicial = int(input("Selecione uma das 3 opções : \n"))
+            if cod_moeda_inicial == 1:
+                cod_moeda_inicial = 'BRL'
+                print("Cod de escolha:",cod_moeda_inicial)
+                break
+            elif cod_moeda_inicial == 2:
+                cod_moeda_inicial = 'USD'
+                print("Cod de escolha:",cod_moeda_inicial)
+                break
+            elif cod_moeda_inicial == 3:
+                cod_moeda_inicial = input('\nOutro, codigo de moeda escolhida:').upper()
+                print("Cod de escolha :",cod_moeda_inicial)
+                break
+            else:
+                mensagem_de_erro()
+        except:
+            mensagem_de_erro()
+
+    formatacao_titulo('SELECIONE A MOEDA DE CAMBIO')
+    cod_moeda_cambio = None
+    while cod_moeda_cambio not in (1,2,3):
+        print_moeda()
+        try:
+            cod_moeda_cambio = int(input("Selecione uma das 3 opções : \n"))
+            if cod_moeda_cambio == 1:
+                cod_moeda_cambio = 'BRL'
+                print("Cod de escolha:",cod_moeda_cambio)
+                break
+            elif cod_moeda_cambio == 2:
+                cod_moeda_cambio = 'USD'
+                print("Cod de escolha:",cod_moeda_cambio)
+                break
+            elif cod_moeda_cambio == 3:
+                cod_moeda_cambio = input('\nOutro, codigo de moeda escolhida:').upper()
+                print("Cod de escolha :",cod_moeda_cambio)
+                break
+            else:
+                mensagem_de_erro()                
+        except:
+            mensagem_de_erro()  
+      
 
 
